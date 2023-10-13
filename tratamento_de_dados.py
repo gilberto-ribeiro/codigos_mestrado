3981ebd7aef189647755c55a4e5f9239238002bb
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-10-13 06:14:21 -0300

    Início do tratamento de dados dos condutivímetros

--- tratamento_de_dados.py
+++ tratamento_de_dados.py
@@ -12,6 +12,8 @@ class Condutivimetro:
     def __init__(self, caminho):
         self._caminho = caminho
         self._obter_arquivo()
+        self._obter_base_de_dados()
+        self._tratar_base_de_dados()
 
     @property
     def caminho(self):
@@ -36,8 +38,19 @@ class Condutivimetro:
     @property
     def eletrodo(self):
         return f'eletrodo_{self.numero_eletrodo}'
+    
+    @property
+    def dados_originais(self):
+        return self._dados_originais
+    
+    @property
+    def dados_tratados(self):
+        return self._dados_tratados
+    
+    @property
+    def temperatura_media(self):
+        return self._dados_tratados['temperatura'].mean()
 
-    @staticmethod
     def _obter_arquivo(self):
         arquivo = os.path.basename(self._caminho)
         if __class__.padrao_csv.search(arquivo):
@@ -46,10 +59,17 @@ class Condutivimetro:
         else:
             pass
     
-    @staticmethod
     def _obter_identificacao(self):
         self._prefixo = __class__.padrao_csv.search(self._arquivo).group(1)
         self._numero_prefixo = __class__.padrao_csv.search(self._arquivo).group(2) 
         self._numero_eletrodo = __class__.padrao_csv.search(self._arquivo).group(4) 
 
-    
+    def _obter_base_de_dados(self):
+        self._dados_originais = pd.read_csv(self._caminho, encoding='latin1', sep=';', decimal=',')
+
+    def _tratar_base_de_dados(self):
+        dados = self._dados_originais.iloc[:, 0:4].copy()
+        colunas_renomeadas = ['data', 'horario', 'condutividade_eletrica', 'temperatura']
+        colunas_mapeadas = {i: j for i, j in zip(dados.columns, colunas_renomeadas)}
+        dados.rename(columns=colunas_mapeadas, inplace=True)
+        self._dados_tratados = dados
